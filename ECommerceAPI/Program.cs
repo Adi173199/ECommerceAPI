99769d19a4a85fb1cb97c94d625402753f9c431f
ADITYA <ADITYA@ADILAPTOP>
2026-05-21 18:56:44 +0530

    Add project files.

---
+++ ECommerceAPI/Program.cs
@@ -0,0 +1,32 @@
+using Microsoft.OpenApi.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("ecommerce", new OpenApiInfo { Title = "ECommerce API", Version = "v1" });
+    c.SwaggerDoc("core", new OpenApiInfo { Title = "Core API", Version = "v1" });
+    c.SwaggerDoc("infrastructure", new OpenApiInfo { Title = "Infrastructure API", Version = "v1" });
+});
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
