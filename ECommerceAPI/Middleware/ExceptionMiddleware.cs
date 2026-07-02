f889567207797562b7aeb100d90087cad8bb9974
Aditya Singh <adityaomsingh.2000@gmail.com>
2026-07-02 12:33:24 +0530

    Commit till section 6

---
+++ ECommerceAPI/Middleware/ExceptionMiddleware.cs
@@ -0,0 +1,35 @@
+﻿using API.Errors;
+using System.Net;
+using System.Text.Json;
+
+namespace API.Middleware
+{
+    public class ExceptionMiddleware(IHostEnvironment env, RequestDelegate next)
+    {
+        public async Task InvokeAsync(HttpContext context)
+        {
+            try
+            {
+                await next(context);
+            }
+            catch (Exception ex)
+            {
+                await HandleExceptionAsync(context, ex, env);
+            }
+        }
+
+        private static Task HandleExceptionAsync(HttpContext context, Exception ex, IHostEnvironment env)
+        {
+            context.Response.ContentType = "application/json";
+            context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+
+            var reponse = env.IsDevelopment() ? new APIErrorResponse(context.Response.StatusCode, ex.Message, ex.StackTrace) : new APIErrorResponse(context.Response.StatusCode, ex.Message, "Internal Server Error");
+
+            var option = new JsonSerializerOptions { PropertyNamingPolicy = JsonNamingPolicy.CamelCase };
+
+            var json = JsonSerializer.Serialize(reponse, option);
+            
+            return context.Response.WriteAsync(json);
+        }
+    }
+}
